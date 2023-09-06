693daddb2dd3ad259317e7422a41100c74a8d4bc
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-09-06 12:24:39 -0400

    Divs setup for logic
    Page body setup - needs logic and CSS framework to function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="playbox">
+        <div className="PlaySpace">
+          <div className="player1">
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+          </div>
+          <div className="linkzone"></div>
+          <div className="player2">
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+            <div className="cardspace"></div>
+          </div>
+        </div>
+      </div>
+      <div className="tracker-Space">
+        <div className="lp-p1"></div>
+        <div className="hand-p1"></div>
+        <div className="util">
+          <Coin></Coin>
+          <div className="trash"></div>
+          <Dice></Dice>
+        </div>
+        <div className="lp-p2"></div>
+        <div className="hand-p2"></div>
+      </div>
+      <div className="search">
+        <Search></Search>
+        <div className="search-result"></div>
+      </div>
     </div>
   );
 }
